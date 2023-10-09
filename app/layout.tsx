149fe7486ffac70c828955b765a5d9100056d935
Anna <anysofronova@yandex.ru>
2023-10-09 10:56:35 +0200

    feat: supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
+import prismadb from '@/lib/prismadb'
 import { ModalProvider } from '@/components/providers/modal-provider'
 
 import './globals.css'
